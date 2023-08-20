0028f56b055009f497491fab0d0b417658e22d51
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2023-08-20 14:27:28 +0000

    variable

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId=123
+
+let accountEmail="nikhil@123"
+
+let accountPassword=321
+var city="surat"
+
+x = 5;
+y = 6;
+z = x + y;
+console.log("The value of z is: " + z)
+
+
+console.log(accountEmail);
+
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+console.log(total);
