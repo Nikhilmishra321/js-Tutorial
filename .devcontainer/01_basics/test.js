7e770edab8ad24ae2d59c41270eb5141ecd9634b
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2023-08-20 20:02:05 +0530

    Update test.js

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,13 @@
 console.log("hello world")
+/*
+In a programming language, variables are used to store data values.
+
+JavaScript uses the keywords var, let and const to declare variables.
+
+An equal sign is used to assign values to variables.
+
+In this example, x is defined as a variable. Then, x is assigned (given) the value 6:
+
+let x;
+
+*/
