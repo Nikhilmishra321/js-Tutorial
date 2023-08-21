654b42b67eb2f09731e5714f5f56a8926017779f
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2023-08-21 10:59:04 +0000

    conversion of datatype

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+// let x=16+"volvo";
+
+let x;
+ x=30;
+ x="volvo";
+console.log(x);
+
+let carname1="Maruti";
+let carname2="Mahendra";
+
+console.log(carname1+carname2)
+
+let y="volvo"+16;
+console.log(y);
+
+//1=>true;0=>false
+//""=>false
+//"hitesh"=>true
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log( typeof stringNumber);
