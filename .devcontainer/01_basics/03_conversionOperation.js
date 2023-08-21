996b6925e059c53c902595ef7718f3772200a3b2
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2023-08-21 21:11:31 +0530

    Update 03_conversionOperation.js

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -21,3 +21,23 @@ let someNumber=33
 
 let stringNumber=String(someNumber)
 console.log( typeof stringNumber);
+
+
+//Operators
+
+let value=5
+let negative=-value
+// console.log(negValue);
+// console.log(2+3);
+// console.log(3-2);
+// console.log(2*3);
+// console.log(2**3);
+
+let str1 ="hello"
+let str2 ="nikhil"
+let str3 =str1+str2
+console.log(str3);
+
+
+
+
