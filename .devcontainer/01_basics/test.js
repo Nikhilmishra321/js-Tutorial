ba47ff0390f350f5fa91db887fc57b73c6194ea2
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2023-08-22 09:48:02 +0530

    Update test.js

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -11,3 +11,20 @@ In this example, x is defined as a variable. Then, x is assigned (given) the val
 let x;
 
 */
+
+// At a restaurant, you order 1 soup for $10, 3 burger for $8 each and 1 ice-cream for  $5 use javaScript   to calculate cost of order
+
+let soupPrice=10
+let burgerPrice=8
+let icePrice=5
+
+//quantity of item
+
+let soupQuantity=1;
+let burgerQuantity=3;
+let iceCreamQuantity=1;
+
+//calculate total
+const totalCost=( soupQuantity*soupPrice)+(burgerQuantity * burgerPrice)+(iceCreamQuantity*iceCreamPrice);
+//print total cost
+console.log('Total cost:$${totalCost}');
