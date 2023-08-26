858b651e4ade948a4e0991c959c22b50f429d2f7
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2023-08-26 11:27:40 +0000

    pratise of variable

---
+++ .devcontainer/01_basics/myScript.js
@@ -0,0 +1,50 @@
+alert("Hello");
+
+// [1, 2].forEach(alert);
+
+//variable
+
+let message;
+
+message="hell0";
+
+message="hello mishra";
+console.log(message);
+
+let user="john"
+let age= "23"
+let message1="world"
+
+console.log('user');
+console.log('age');
+
+// Constant
+// To declare a constant (unchanging) variable, use const instead of let:
+
+// const name="nikhil";
+// const birthDate=18-2-2002;
+const myCurrentAge=22;
+
+console.log(name,birthDate,myCurrentAge);
+
+// summary
+// We can declare variables to store data by using the var, let, or const keywords.
+
+// let – is a modern variable declaration.
+// var – is an old-school variable declaration. Normally we don’t use it at all, but we’ll cover subtle differences from let in the chapter The old "var", just in case you need them.
+// const – is like let, but the value of the variable can’t be changed.
+
+// Task 
+
+// Working with variable
+
+// 1.Declare two variables: admin and name.
+
+ let admin , name;
+
+  name ="john";
+
+  admin="name";
+
+  alert(admin);
+
