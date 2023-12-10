5d18f34bcf8b0e07026c97c71976ab0a8b3225f0
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2023-12-10 19:16:07 +0530

    comparing of values

---
+++ .devcontainer/02_basics/2.4_Comparsion.js
@@ -0,0 +1,22 @@
+// Greater/less than: a > b, a < b.
+// Greater/less than or equals: a >= b, a <= b.
+// Equals: a == b, please note the double equality sign == means the equality test, while a single one a = b means an assignment.
+// Not equals: In maths the notation is ≠, but in JavaScript it’s written as a != b.
+
+alert(2>1);//true
+
+alert(2==1);//false
+
+alert(2!=1);//true
+
+//compare result of variable
+
+let result = 5 > 4; // assign the result of the comparison
+alert( result ); // true
+
+
+// For boolean values, true becomes 1 and false becomes 0.
+
+alert( true == 1 ); // true
+alert( false == 0 ); // true
+
