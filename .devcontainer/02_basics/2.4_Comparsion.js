f8fc6b58e8a230e25448adc6a61978bebe3aed82
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2023-12-10 19:41:08 +0530

    if and else condition used

--- .devcontainer/02_basics/2.4_Comparsion.js
+++ .devcontainer/02_basics/2.4_Comparsion.js
@@ -20,3 +20,5 @@ alert( result ); // true
 alert( true == 1 ); // true
 alert( false == 0 ); // true
 
+
+
---
+++ .devcontainer/02_basics/2.5_Conditional.js
@@ -0,0 +1,39 @@
+// if condition
+
+// The if(...) statement evaluates a condition in parentheses and, if the result is true, executes a block of code.
+
+let year=prompt("in which year was esmaScript specification","");
+
+if(year==2015) {
+    alert("you are right");
+
+}
+
+let age =18;
+
+if(age>=15){
+    console.log(age);
+}
+
+//truthy and falsy value
+
+if(1){
+    console.log(1);
+}
+
+if(0){
+    console.log(0)
+}
+
+//else
+
+let year1= prompt('In which year was the ECMAScript-2015 specification published?', '');
+
+if(year1==2015){
+    alert("you gussed its right");
+}else{
+    alert("how can be so wrong");
+}
+
+
+ 
