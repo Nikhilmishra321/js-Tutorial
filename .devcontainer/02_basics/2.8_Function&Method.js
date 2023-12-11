2177043c422b7fc63229efbf89b7e538dd25b3c3
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2023-12-11 11:27:27 +0530

    sum of two number using function

---
+++ .devcontainer/02_basics/2.8_Function&Method.js
@@ -0,0 +1,17 @@
+//Function is a block of code that perform specific task and allow the code of many times without reptition
+
+//function Definition
+
+// function myFunction(){
+//     console.log("welcome to india");
+//     console.log("we are learning JS");
+// }
+
+// myFunction();
+
+function sum(x,y){
+    s=x+y;
+    return s;
+}
+let val=sum(3,4);
+console.log(val);
