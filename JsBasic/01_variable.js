01d1d6b787487703027ea3308e10220bc8f77947
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-03-01 16:34:02 +0530

    variable

---
+++ JsBasic/01_variable.js
@@ -0,0 +1,11 @@
+const accountId=23;
+let acountEmail="nikkhilmishra";
+var accountPassword="12345";
+
+// accountId=34;
+
+console.log(accountId);
+
+console.log(acountEmail);
+console.log(accountPassword);
+
