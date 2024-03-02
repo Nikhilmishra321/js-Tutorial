0e3fc17f70c29327b0f95ebf0c3ea65aeeac0c89
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-03-02 16:33:51 +0530

    type conversion

---
+++ JsBasic/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score="33";
+
+console.log(typeof(score))
+
+let valueName= Number(score)
+
+console.log(valueName)
+
+console.log(typeof valueName)
+
+
+let isBoolean="shran";
+
+let isBooleanLog= Boolean(isBoolean)
+
+console.log(isBoolean);
+
+let isNumber=33;
+
+let stringNumber=isNumber;
+
+console.log(stringNumber);
