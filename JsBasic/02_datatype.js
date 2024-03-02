75065d6cf8f404a847b18a94df89a10ad408e62f
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-03-02 10:05:35 +0530

    datatype and ecmaScript

--- JsBasic/02_datatype.js
+++ JsBasic/02_datatype.js
@@ -1,3 +1,15 @@
 "use strict"; //treat all js code in newer version
 
 alert(3+3); //we are using node js not browser
+
+let name="hitesh";
+let age=18;
+let isLoggedIn=false;
+
+//number = 2 to power 53
+//bigInt
+//null standalone value
+//undefind  variable will be assigned but not assigned value
+//type of check the type of variable
+
+console.log(typeof "hitesh");
