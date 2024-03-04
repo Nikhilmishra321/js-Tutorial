68e8f3f1b62e41baffc55da36346e350cffa418d
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-03-04 16:04:43 +0530

    data type summary

---
+++ JsBasic/datatype-summary.js
@@ -0,0 +1,20 @@
+//primtive
+
+//7types :String, Number,Booean,null,undefind,Symbol,BigInt
+
+//Non primtive
+
+//Arrays,object,Function
+
+const heroes=["shakiman","hathim"]
+
+let myObject={
+    name:"ram",
+    class:"bca"
+}
+
+let myFunction=function(){
+    console.log("Hello world")
+
+}
+console.log(typeof myFunction)
