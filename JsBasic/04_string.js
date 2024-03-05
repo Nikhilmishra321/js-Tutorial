b8c02de1c74c9286cb89f040302258a3b1537905
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-03-05 08:36:17 +0530

    string method

--- JsBasic/04_string.js
+++ JsBasic/04_string.js
@@ -4,3 +4,14 @@ const repoCount= 50
 //console.log(name+repoCount + "value")
 
 console.log(`Hello my name is ${name} and my repoCount is a ${repoCount}`);
+
+const gameName=new String('shyam')
+
+console.log(gameName[0]);
+
+console.log(gameName.length)
+console.log(gameName.toUpperCase)
+console.log(gameName.indexOf('y'))
+
+const newString="  hitesh"
+console.log(newString.trim())
