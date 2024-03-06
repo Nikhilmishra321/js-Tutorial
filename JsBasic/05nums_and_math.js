16e65fac11b351f8b6fad933a30014c10297524b
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-03-06 11:35:38 +0530

    maths

--- JsBasic/05nums_and_math.js
+++ JsBasic/05nums_and_math.js
@@ -8,3 +8,15 @@ console.log(balence.toString);
 
 const hunderd=10000;
 console.log(hunderd.toLocaleString('en-IN'));
+
+// ==============================Maths===========================================
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.ceil(4.3));
+console.log(Math.round(5.3));
+console.log(Math.floor(7.3));
+console.log(Math.min(5,4,8,3,7));
+
+
+
