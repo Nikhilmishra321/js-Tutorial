a8ae6ae748a54c4fb5c6f8c4b3ad31bb86d1d845
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-03-06 11:21:37 +0530

    nums

---
+++ JsBasic/05nums_and_math.js
@@ -0,0 +1,10 @@
+const score=400;
+console.log(score);
+
+const balence=new Number(100);
+console.log(balence);
+
+console.log(balence.toString);
+
+const hunderd=10000;
+console.log(hunderd.toLocaleString('en-IN'));
