1e64531cf4b4ea974f9afb1c6d5a3cf8c40885a0
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-03-07 16:42:39 +0530

    acess array element

--- jsBasic2/01_arrays.js
+++ jsBasic2/01_arrays.js
@@ -10,3 +10,22 @@ const myArr2=new  Array(1,2,3,4);
 
 
 console.log(myArr)
+
+//accesing Element
+
+const hello='Hello world';
+console.log(hello[6]);
+
+
+
+//length property
+
+const number=[1,2,3,4,5];
+console.log(number.length);
+
+//push()
+
+const cart=['apple','orange'];
+
+cart.push('pear');
+console.log(cart);
---
+++ jsBasic2/02_arrays.js
@@ -0,0 +1,11 @@
+//This code accesses the value of the first element in the fruit array:
+
+const fruit=['apple','banana','orange'];
+console.log(fruit[0]);
+
+//Array elements can be changed by accessing the element and assigning a new value to it.
+
+const heroes=['shaktiaman','krish','spiderman'];
+heroes[1]='hathim';
+console.log(heroes[1]);
+
