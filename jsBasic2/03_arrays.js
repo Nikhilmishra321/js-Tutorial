ee410ed33d840fd90f4fe7f68816eafb61d495ca
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-03-08 12:51:23 +0530

    looping of arrays

---
+++ jsBasic2/03_arrays.js
@@ -0,0 +1,26 @@
+// let marks=[60,70,50,80];
+// console.log(marks);
+// console.log(marks.length);
+// console.log(typeof marks);
+
+//indicing of arrays
+
+// marks[1]=84;
+// console.log(marks)
+
+//looping 
+
+// for loop
+let heroes=["ironman","thor","hulk","shaktiman","spiderman"];
+
+for(let idx=0; idx<heroes.length; idx++){
+    console.log(heroes[idx]);
+}
+
+//for of loops
+
+let citeies=["curat","patna","darbhanga","bihar"];
+
+for(let city of citeies){
+    console.log(city.toUpperCase());
+}
