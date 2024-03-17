ba4cc3fbffb28d5797232adb77725fe1ab451739
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-03-17 17:59:51 +0530

    object

---
+++ jsObject/02_object.js
@@ -0,0 +1,18 @@
+const tinderUser = new Object()
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email:"nikhil@mishra",
+    name:"Nikhil",
+    class:"tybca",
+    fullName:{
+        userName:{
+            userfullName:"Nikhil Mishra",
+
+        }
+
+    }
+
+}
+console.log(regularUser.fullName.userName);
